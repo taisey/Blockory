2fa76e1a33297f333686ec22890d407e525f57df
taisey <taisey02@gmail.com>
2022-05-15 16:23:46 +0900

    [add]local and prod

--- backend/src/main.go
+++ backend/src/main.go
@@ -10,10 +10,17 @@ import (
 func main() {
 	//MysqlDB設定
 	if os.Args[1] == "local"{
+		//ローカル環境
 		db.InitLocal()
-	}else{
-		db.Init()
 	}
+	if os.Args[1] == "prod"{
+		//引数から必要な情報を取得
+		userName := os.Args[2] 
+		password := os.Args[3]
+		host := os.Args[4]
+		databaseName := os.Args[5]
+		db.InitProd(userName, password, host, databaseName)
+	}	
 	dbInstance := db.GetDB()
 	defer dbInstance.Close()
 
