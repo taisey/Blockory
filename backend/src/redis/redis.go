431396065c12bb8089ffb2344c4090607e11c9fc
taisey <taisey02@gmail.com>
2022-05-25 22:12:17 +0900

    [add] redis

---
+++ backend/src/redis/redis.go
@@ -0,0 +1,44 @@
+package redis
+
+import(
+	"github.com/gomodule/redigo/redis"
+)
+
+var Con redis.Conn
+
+func InitLocal() {
+    const Addr = "localhost:6379"
+
+	var err error
+    Con, err = redis.Dial("tcp", Addr)
+    if err != nil {
+        panic(err)
+    }
+}
+
+func InitProd() {
+    const Addr = "localhost:6379"
+
+	var err error
+    Con, err = redis.Dial("tcp", Addr)
+    if err != nil {
+        panic(err)
+    }
+}
+
+// データの登録(Redis: SET key value)
+func Set(key, value string) string{
+    res, err := redis.String(Con.Do("SET", key, value))
+    if err != nil {
+        panic(err)
+    }
+    return res
+}
+// データの取得(Redis: GET key)
+func Get(key string) string {
+    res, err := redis.String(Con.Do("GET", key))
+    if err != nil {
+        panic(err)
+    }
+    return res
+}
