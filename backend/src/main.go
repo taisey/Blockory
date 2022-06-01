3a2e3054e5f7d75da580a8d826a8ec5c4cbaa59c
taisey <taisey02@gmail.com>
2022-06-01 22:12:45 +0900

    [add] cors

--- backend/src/main.go
+++ backend/src/main.go
@@ -4,13 +4,53 @@ import (
 	"github.com/gin-gonic/gin"
 	"mypkg/api"
 	"mypkg/db"
+	"mypkg/redis"
+	"github.com/gin-contrib/cors"
 	"os"
+	"time"
 )
+func corsSetting(engine *gin.Engine){
+	engine.Use(cors.New(cors.Config{
+		AllowOrigins: []string{
+			"https://example.com",
+			"https://example2.com",
+		},
+		AllowMethods: []string{
+			"POST",
+			"GET",
+			"OPTIONS",
+		},
+		AllowHeaders: []string{
+			"Access-Control-Allow-Credentials",
+			"Access-Control-Allow-Headers",
+			"Content-Type",
+			"Content-Length",
+			"Accept-Encoding",
+			"Authorization",
+		},
+		AllowCredentials: true,
+		MaxAge: 24 * time.Hour,
+	  }))
+}
 
+func corsSettingAllPass(engine *gin.Engine){
+	engine.Use(cors.New(cors.Config{
+		AllowOrigins: []string{
+			"*",
+		},
+		AllowMethods: []string{
+			"*",
+		},
+		AllowHeaders: []string{
+			"*",
+		},
+	  }))
+}
 func main() {
 	//MysqlDB設定
 	if os.Args[1] == "local"{
 		//ローカル環境
+		redis.InitLocal()
 		db.InitLocal()
 	}
 	if os.Args[1] == "prod"{
@@ -19,6 +59,7 @@ func main() {
 		password := os.Args[3]
 		host := os.Args[4]
 		databaseName := os.Args[5]
+		redis.InitProd()
 		db.InitProd(userName, password, host, databaseName)
 	}	
 	dbInstance := db.GetDB()
@@ -26,9 +67,14 @@ func main() {
 
 	//Gin　engine設定
 	engine := gin.Default()
+	//CORS設定
+	//corsSetting(engine)
+	corsSettingAllPass(engine)
+
 	engine.GET("/", api.RootHandle)
 	engine.GET("/DiaryInfo", api.GetDiaryInfo)
-	//engine.POST("/DiaryInfo", api.postDiaryInfo)
+	engine.POST("/DiaryInfo", api.PostDiaryInfo)
+	engine.GET("/AuthUserInfo", api.AuthUserInfo)
 	//engine.GET("/UserInfo", api.getUserInfo)
 	engine.Run(":8080")
 
